648abc20d58600376cfdfdb623b6be50b4f3bd3a
Leonardo Vinicius <152725239+Levidevbr@users.noreply.github.com>
2026-07-14 14:01:33 -0300

    Exercicio 14.07

---
+++ Ex4/Index2.py
@@ -0,0 +1,6 @@
+n1 = int(input('Adicione um valor:'))
+n2 = int(input('Adicione outro valor:'))
+s = n1 + n2
+m = n1 * n2
+d = n1 / n2
+print('A soma dos valores é {}, o produto é {} e a divisão é {}   ' .format(s, m, d))
