c2a4472de438ac203bb8d459d2f259bb1d94b4d8
Leonardo Vinicius <152725239+Levidevbr@users.noreply.github.com>
2026-07-27 16:00:49 -0300

    Exercicio 27.07

---
+++ Desafios/Desafio 38.py
@@ -0,0 +1,8 @@
+vl_1 = int(input('Primeiro numero: '))
+vl_2 = int(input('Segundo numero: '))
+if vl_1 > vl_2:
+    print(f'O primeiro numero({vl_1}) digitado é maior! ')
+elif vl_2 > vl_1:
+    print(f'O segundo numero({vl_2}) digitado é maior! ')
+else:
+    print('Não possui numero maior, os dois valores são igual! ')
