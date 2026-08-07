230f5c5247b521c26ef298d301bdd2517c31d104
Leonardo Vinicius <152725239+Levidevbr@users.noreply.github.com>
2026-08-07 14:08:30 -0300

    Exercicio 7.07

--- Desafios/Desafio 58.py
+++ Desafios/Desafio 58.py
@@ -3,6 +3,7 @@ import random
 lista = [0,1,2,3,4,5,6,7,8,9,10]
 jogador = -1
 escolhido = random.choice(lista)
+tentativa = 0
 
 print("Sou seu computador...")
 print("Acabei de pensar em um numero entre 0 a 10.")
@@ -10,7 +11,18 @@ print("Será que você consegue adivinhar qual foi?")
 
 while jogador != escolhido:
     jogador = int(input("Qual é seu palpite? "))
-    if jogador == escolhido:
-        print("Você acertou!")
+    tentativa += 1
+
+    if jogador != escolhido:
+
+        print("Você errou!")
+
+        if jogador < escolhido:
+            print("Digite um numero mais alto!")
+
+        elif jogador > escolhido:
+            print("Digite um numero mais baixo!")
+            
     else:
-        print("Tente novamente!")
+        print("Você acertou!")
+        print(f"Foi necessario {tentativa} jogadas.")
